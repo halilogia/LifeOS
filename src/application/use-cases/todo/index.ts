3f679b7d01bcf1834241b244c85e46e08e8d30c8
Halil Emre Kuyupinar <halilemrekuyupinar@proton.me>
2026-07-30 14:01:38 +0300

    feat(arch): add barrel exports for clean architecture layers
    Establish proper module organization with barrel index files across
    domain (entities, repositories, services, value-objects), application
    (ports, use-cases), and infrastructure (persistence) layers. Also
    removes outdated clean code violation report and adjusts base CSS
    layout properties.

---
+++ src/application/use-cases/todo/index.ts
@@ -0,0 +1,8 @@
+export { AddTodoUseCase } from "./AddTodoUseCase.js";
+export type { AddTodoRequest, AddTodoResponse } from "./AddTodoUseCase.js";
+export { ToggleTodoUseCase } from "./ToggleTodoUseCase.js";
+export type { ToggleTodoRequest, ToggleTodoResponse } from "./ToggleTodoUseCase.js";
+export { DeleteTodoUseCase } from "./DeleteTodoUseCase.js";
+export { MoveTaskUseCase } from "./MoveTaskUseCase.js";
+export { UpdatePrioritiesUseCase } from "./UpdatePrioritiesUseCase.js";
+export { ResetRepeatingTodosUseCase } from "./ResetRepeatingTodosUseCase.js";
---
+++ src/domain/entities/index.ts
@@ -0,0 +1,2 @@
+export type { Todo } from "./Todo.js";
+export { createTodo, toggleTodo, updateTodoStatus, updateTodoPriorities, assignTodoId } from "./Todo.js";
